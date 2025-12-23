77e80bc18e9b99e1b6cd7d54d4f3715cd60a2693
OFFICIAL GURU <hapawapi0@gmail.com>
2025-12-23 11:01:24 +0300

    X GURU

--- config.js
+++ config.js
@@ -4,48 +4,81 @@ if (fs.existsSync('.env'))
 const path = require("path");
 
 module.exports = { 
-    SESSION_ID: process.env.SESSION_ID, // Session ID must be put in .env to avoid loss during updating
+    SESSION_ID: process.env.SESSION_ID,
+
     PREFIX: process.env.PREFIX || ".",
-    OWNER_NAME: process.env.OWNER_NAME || "𝐆𝐈𝐅𝐓𝐄𝐃 𝐓𝐄𝐂𝐇",
-    OWNER_NUMBER : process.env.OWNER_NUMBER || "",  // put only one number
-    SUDO_NUMBERS : process.env.SUDO_NUMBERS || "", // can be multiple numbers separated by commas
-    BOT_NAME : process.env.BOT_NAME || '𝐆𝐈𝐅𝐓𝐄𝐃 𝐌𝐃',
-    FOOTER : process.env.FOOTER || 'ᴘᴏᴡᴇʀᴇᴅ ʙʏ ɢɪғᴛᴇᴅ ᴛᴇᴄʜ',
-    CAPTION : process.env.CAPTION || '©𝟐𝟎𝟐𝟒 𝐆𝐈𝐅𝐓𝐄𝐃 𝐌𝐃 𝐕𝟓',
-    VERSION: process.env.VERSION || '5.0.0',
-    BOT_PIC : process.env.BOT_PIC || 'https://gitcdn.giftedtech.co.ke/image/AZO_image.jpg',            
+
+    OWNER_NAME: process.env.OWNER_NAME || "GuruTech",
+    OWNER_NUMBER: process.env.OWNER_NUMBER || "254704355518",
+    SUDO_NUMBERS: process.env.SUDO_NUMBERS || "254704355518",
+
+    BOT_NAME: process.env.BOT_NAME || "X GURU",
+    FOOTER: process.env.FOOTER || "Powered by X GURU",
+    CAPTION: process.env.CAPTION || "© 2025 X GURU",
+
+    VERSION: process.env.VERSION || "5.0.0",
+
+    BOT_PIC: process.env.BOT_PIC || "https://files.catbox.moe/7czrlj.jpg",
+
     MODE: process.env.MODE || "public",
-    PM_PERMIT: process.env.PM_PERMIT || 'false',
-    WARN_COUNT : process.env.WARN_COUNT || '3' ,
+    PM_PERMIT: process.env.PM_PERMIT || "false",
+    WARN_COUNT: process.env.WARN_COUNT || "3",
+
     TIME_ZONE: process.env.TIME_ZONE || "Africa/Nairobi",
-    DM_PRESENCE : process.env.DM_PRESENCE || 'online', // recording/typing/online/offline
-    GC_PRESENCE : process.env.GC_PRESENCE || 'online', // recording/typing/online/offline
-    CHATBOT : process.env.CHATBOT || 'false', // can be true/audio/false   
-    CHATBOT_MODE : process.env.CHATBOT_MODE || 'inbox', // can be inbox/groups/allchats
-    STARTING_MESSAGE : process.env.STARTING_MESSAGE || "true",
-    ANTIDELETE : process.env.ANTIDELETE || 'indm', // inchat/indm/false
-    GOODBYE_MESSAGE : process.env.GOODBYE_MESSAGE || 'false',
-    ANTICALL : process.env.ANTICALL || 'false', // (decline/true)/block/false
-    ANTICALL_MSG: process.env.ANTICALL_MSG || "*_📞 Auto Call Reject Mode Active. 📵 No Calls Allowed!_*",
-    WELCOME_MESSAGE : process.env.WELCOME_MESSAGE || 'false',
-    ANTILINK : process.env.ANTILINK || 'false', // or delete or kick or true
-    AUTO_LIKE_STATUS : process.env.AUTO_LIKE_STATUS || 'true',
-    AUTO_READ_STATUS : process.env.AUTO_READ_STATUS || 'true',
-    STATUS_LIKE_EMOJIS : process.env.STATUS_LIKE_EMOJIS || "💛,❤️,💜,🤍,💙",
-    AUTO_REPLY_STATUS: process.env.AUTO_REPLY_STATUS || "false",   
-    STATUS_REPLY_TEXT: process.env.STATUS_REPLY_TEXT || "*ʏᴏᴜʀ sᴛᴀᴛᴜs ᴠɪᴇᴡᴇᴅ sᴜᴄᴄᴇssғᴜʟʟʏ ✅*",             
-    AUTO_REACT : process.env.AUTO_REACT || 'false',
-    AUTO_REPLY : process.env.AUTO_REPLY || 'false',
-    AUTO_READ_MESSAGES : process.env.AUTO_READ_MESSAGES || 'false', // true/commands/false
-    AUTO_BIO : process.env.AUTO_BIO || 'false',
-    AUTO_BLOCK: process.env.AUTO_BLOCK || '',
-    YT: process.env.YT || 'youtube.com/@giftedtechnexus',
-    NEWSLETTER_JID: process.env.NEWSLETTER_JID || "120363425418645942@newsletter",
+
+    DM_PRESENCE: process.env.DM_PRESENCE || "online",
+    GC_PRESENCE: process.env.GC_PRESENCE || "online",
+
+    CHATBOT: process.env.CHATBOT || "false",
+    CHATBOT_MODE: process.env.CHATBOT_MODE || "inbox",
+
+    STARTING_MESSAGE: process.env.STARTING_MESSAGE || "true",
+
+    ANTIDELETE: process.env.ANTIDELETE || "indm",
+
+    GOODBYE_MESSAGE: process.env.GOODBYE_MESSAGE || "false",
+
+    ANTICALL: process.env.ANTICALL || "false",
+    ANTICALL_MSG:
+      process.env.ANTICALL_MSG ||
+      "*_📞 Auto Call Reject Mode Active. 📵 No Calls Allowed!_*",
+
+    WELCOME_MESSAGE: process.env.WELCOME_MESSAGE || "false",
+
+    ANTILINK: process.env.ANTILINK || "false",
+
+    AUTO_LIKE_STATUS: process.env.AUTO_LIKE_STATUS || "true",
+    AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+    STATUS_LIKE_EMOJIS: process.env.STATUS_LIKE_EMOJIS || "💛,❤️,💜,🤍,💙",
+
+    AUTO_REPLY_STATUS: process.env.AUTO_REPLY_STATUS || "false",
+    STATUS_REPLY_TEXT:
+      process.env.STATUS_REPLY_TEXT ||
+      "*ʏᴏᴜʀ sᴛᴀᴛᴜs ᴠɪᴇᴡᴇᴅ sᴜᴄᴄᴇssғᴜʟʟʏ ✅*",
+
+    AUTO_REACT: process.env.AUTO_REACT || "false",
+    AUTO_REPLY: process.env.AUTO_REPLY || "false",
+    AUTO_READ_MESSAGES: process.env.AUTO_READ_MESSAGES || "false",
+
+    AUTO_BIO: process.env.AUTO_BIO || "false",
+    AUTO_BLOCK: process.env.AUTO_BLOCK || "",
+
+    YT: process.env.YT || "youtube.com/@XGURU",
+
+    NEWSLETTER_JID:
+      process.env.NEWSLETTER_JID || "120363421164015033@newsletter",
+
     GC_JID: process.env.GC_JID || "GiD4BYjebncLvhr0J2SHAg",
-    NEWSLETTER_URL: process.env.NEWSLETTER_URL || 'https://whatsapp.com/channel/0029VbC5WlPL7UVQ6AbK7x2n',
-    BOT_REPO: process.env.BOT_REPO || 'mauricegift/gifted-md',
-    PACK_NAME: process.env.PACK_NAME || '𝐆𝐈𝐅𝐓𝐄𝐃 𝐌𝐃',
-    PACK_AUTHOR: process.env.PACK_AUTHOR || '𝐆𝐈𝐅𝐓𝐄𝐃 𝐓𝐄𝐂𝐇'
+
+    NEWSLETTER_URL:
+      process.env.NEWSLETTER_URL ||
+      "https://whatsapp.com/channel/0029VbC5WlPL7UVQ6AbK7x2n",
+
+    BOT_REPO:
+      process.env.BOT_REPO || "https://github.com/ADDICT-HUB/XGURU-",
+
+    PACK_NAME: process.env.PACK_NAME || "X GURU",
+    PACK_AUTHOR: process.env.PACK_AUTHOR || "GuruTech"
 };
 
 let fileName = require.resolve(__filename);
